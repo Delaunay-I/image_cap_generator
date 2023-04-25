b8854f397af92bb79d75a93c36f73040be5b2f0c
Amirhossein <amirmsh@student.ubc.ca>
2023-04-24 22:02:50 -0700

    completed test web app

--- web/app.py
+++ web/app.py
@@ -39,6 +39,30 @@ def index():
         # Show the page
         return render_template("index.html", tasks=tasks)
     
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return "There was a problrm deleting that task"
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+    if request.method == "POST":
+        task.content = request.form['content']
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "There was a problrm updating the task"
+    else:
+        return render_template('update.html', task=task)
+
 
 if __name__ == "__main__":
     # set up debug mode, so all errors show up in the web app
