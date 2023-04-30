6c8a08f953c1a09fae363dd49c7372da819f5406
Amirhossein <amirmsh@student.ubc.ca>
2023-04-30 15:52:19 -0700

    added error 500

--- web_app/app/errors.py
+++ web_app/app/errors.py
@@ -4,8 +4,12 @@ from app import app
 
 @app.errorhandler(413)
 def file_too_big(error):
-    return render_template('413.html')
+    return render_template('413.html'), 413
 
 @app.errorhandler(400)
 def file_not_compatible(error):
-    return render_template('400.html')
+    return render_template('400.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html'), 500
