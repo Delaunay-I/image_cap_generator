043244a47074373d86e5264652384e6af2bf929e
Amirhossein <amirmsh@student.ubc.ca>
2023-04-30 10:50:57 -0700

    cleaned the code

--- web_app/app/routes.py
+++ web_app/app/routes.py
@@ -4,14 +4,6 @@ from app.forms import ImageForm
 from werkzeug.utils import secure_filename
 import os
 
-# @app.route('/', methods = ['POST', 'GET'])
-# def index():
-#     if request.method == 'POST':
-#         uploaded_file = request.files['file']
-#         if uploaded_file.filename != '':
-#             uploaded_file.save(uploaded_file.filename)
-#         return redirect(url_for('index'))
-#     return render_template('index.html')
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
