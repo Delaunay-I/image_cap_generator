4294d65cfe2c67057a666b86fa39fb53de45349e
Amirhossein <amirmsh@student.ubc.ca>
2023-04-30 13:58:01 -0700

    changed the file content checker format
    from imghdr module to hex method

--- web_app/app/forms.py
+++ web_app/app/forms.py
@@ -2,25 +2,34 @@ from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed, FileRequired
 from wtforms import SubmitField
 from wtforms.validators import ValidationError
-import imghdr
 
 
-def validate_image(stream):
-    header = stream.read(512)
+def validate_image_content(stream):
+    # Read the first 4 bytes of the file
+    header = stream.read(4)
+    # Reset the stream position
     stream.seek(0)
-    format = imghdr.what(None, header)
-    if not format:
+    # Convert the bytes to hex format
+    hex_header = header.hex().upper()
+    # Check if the hex header matches any of the image formats
+    if hex_header.startswith('FFD8FF'):
+        return '.jpg'
+    elif hex_header.startswith('89504E47'):
+        return '.png'
+    else:
         return None
-    return '.' + (format if format != 'jpeg' else 'jpg')
 
 class ImageForm(FlaskForm):
     image = FileField('image', validators=[
         FileRequired(message='File is required!'),
-        FileAllowed(['jpg', 'png', 'tiff'], 'Images only!')
+        FileAllowed(['jpg', 'png'], 'Images only!')
         ])
     submit = SubmitField('Submit')
 
     def validate_image(self, image):
-        content_type = validate_image(image.data.stream)
+        content_type = validate_image_content(image.data.stream)
         if content_type is None:
             raise ValidationError('File is not a valid image.')
+        # Add this check for file extension
+        if content_type not in ['.jpg', '.png']:
+            raise ValidationError('Images only!')
