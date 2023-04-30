2962be3e06382b53376e8aca500910301878910b
Amirhossein <amirmsh@student.ubc.ca>
2023-04-30 15:52:38 -0700

    upload folder now gives abs path

--- web_app/config.py
+++ web_app/config.py
@@ -4,12 +4,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     WTF_CSRF_SECRET_KEY = 'a random string'
-    UPLOAD_FOLDER = "uploads"
+    UPLOAD_FOLDER = os.path.join(basedir, "uploads")
     MAX_CONTENT_LENGTH = 1024 * 1024
 
     if not os.path.exists(UPLOAD_FOLDER):
         os.makedirs(UPLOAD_FOLDER)
-
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
