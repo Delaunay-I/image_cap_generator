3c8fb0517d4293bbd55daff20ea7632d1bbf1f2c
Amirhossein <amirmsh@student.ubc.ca>
2023-04-30 12:47:38 -0700

    changed image upload directory

--- web_app/config.py
+++ web_app/config.py
@@ -4,7 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     WTF_CSRF_SECRET_KEY = 'a random string'
-    UPLOAD_FOLDER = os.path.join(basedir, 'test_images')
+    UPLOAD_FOLDER = "uploads"
     MAX_CONTENT_LENGTH = 1024 * 1024
 
     if not os.path.exists(UPLOAD_FOLDER):
