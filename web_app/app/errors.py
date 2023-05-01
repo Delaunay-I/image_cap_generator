3ffab92511bd318434d812a30f6d072a449aa600
Amirhossein <amirmsh@student.ubc.ca>
2023-04-30 18:15:25 -0700

    added 404 error

--- web_app/app/errors.py
+++ web_app/app/errors.py
@@ -2,13 +2,19 @@ from flask import render_template
 from app import app
 
 
-@app.errorhandler(413)
-def file_too_big(error):
-    return render_template('413.html'), 413
 
 @app.errorhandler(400)
 def file_not_compatible(error):
-    return render_template('400.html'), 404
+    return render_template('400.html'), 400
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(413)
+def file_too_big(error):
+    return render_template('413.html'), 413
 
 @app.errorhandler(500)
 def internal_error(error):
